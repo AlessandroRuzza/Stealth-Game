b1d6520690951fd1fef4964b3ceaa105087d66cc
Aldo Gordoni <2016736@swansea.ac.uk>
2023-05-12 10:31:07 +0100

    GAME FINISHED!!!!!

--- Assets/Prefabs/Main Menu/Difficulty.cs
+++ Assets/Prefabs/Main Menu/Difficulty.cs
@@ -10,13 +10,17 @@ public class Difficulty : MonoBehaviour
     public const string keyDifficulty = "Difficulty";
     [SerializeField] int difficulty;
     [SerializeField] Color selectedColor;
-    Image image; 
+    Image image;
+
     string playerFolderPath
     {
         get { return Application.persistentDataPath + "/" + PlayerPrefs.GetString(ConfirmName.keyPlayerName) + "/"; }
     }
 
     public static Action reload = null;
+
+    // State pattern for difficulty
+    private IDifficultyState difficultyState;
 
     private void Awake()
     {
@@ -51,17 +55,67 @@ public class Difficulty : MonoBehaviour
 
         try
         {
-            File.Delete(playerFolderPath + "Difficulty_1");
-            File.Delete(playerFolderPath + "Difficulty_2");
-            File.Delete(playerFolderPath + "Difficulty_3");
-            File.Create(playerFolderPath + "Difficulty_" + difficulty).Close();
+            // Set the difficulty state based on the selected difficulty
+            switch (difficulty)
+            {
+                case 1:
+                    difficultyState = new Difficulty1();
+                    break;
+                case 2:
+                    difficultyState = new Difficulty2();
+                    break;
+                case 3:
+                    difficultyState = new Difficulty3();
+                    break;
+                default:
+                    break;
+            }
+
+            // Save the selected difficulty
+            difficultyState.SaveDifficulty(playerFolderPath, difficulty);
         }
-        catch (IOException e) {
+        catch (IOException e)
+        {
             Debug.LogError("Player not initialised! " + e.Message);
-            return; 
+            return;
         }
 
         PlayerPrefs.SetInt(keyDifficulty, difficulty);
         reload();
+    }
+}
+
+public interface IDifficultyState
+{
+    void SaveDifficulty(string playerFolderPath, int difficulty);
+}
+
+public class Difficulty1 : IDifficultyState
+{
+    public void SaveDifficulty(string playerFolderPath, int difficulty)
+    {
+        File.Delete(playerFolderPath + "Difficulty_2");
+        File.Delete(playerFolderPath + "Difficulty_3");
+        File.Create(playerFolderPath + "Difficulty_1").Close();
+    }
+}
+
+public class Difficulty2 : IDifficultyState
+{
+    public void SaveDifficulty(string playerFolderPath, int difficulty)
+    {
+        File.Delete(playerFolderPath + "Difficulty_1");
+        File.Delete(playerFolderPath + "Difficulty_3");
+        File.Create(playerFolderPath + "Difficulty_2").Close();
+    }
+}
+
+public class Difficulty3 : IDifficultyState
+{
+    public void SaveDifficulty(string playerFolderPath, int difficulty)
+    {
+        File.Delete(playerFolderPath + "Difficulty_1");
+        File.Delete(playerFolderPath + "Difficulty_2");
+        File.Create(playerFolderPath + "Difficulty_3").Close();
     }
 }
--- Assets/Prefabs/Player/PlayerMover.cs
+++ Assets/Prefabs/Player/PlayerMover.cs
@@ -3,6 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public interface ICommand
+{
+    void Execute();
+}
+
+public class MoveUpCommand : ICommand
+{
+    private PlayerMover _playerMover;
+
+    public MoveUpCommand(PlayerMover playerMover)
+    {
+        _playerMover = playerMover;
+    }
+
+    public void Execute()
+    {
+        _playerMover.Force += Vector2.up;
+    }
+}
+
+public class MoveDownCommand : ICommand
+{
+    private PlayerMover _playerMover;
+
+    public MoveDownCommand(PlayerMover playerMover)
+    {
+        _playerMover = playerMover;
+    }
+
+    public void Execute()
+    {
+        _playerMover.Force += Vector2.down;
+    }
+}
+
+public class MoveLeftCommand : ICommand
+{
+    private PlayerMover _playerMover;
+
+    public MoveLeftCommand(PlayerMover playerMover)
+    {
+        _playerMover = playerMover;
+    }
+
+    public void Execute()
+    {
+        _playerMover.Force += Vector2.left;
+    }
+}
+
+public class MoveRightCommand : ICommand
+{
+    private PlayerMover _playerMover;
+
+    public MoveRightCommand(PlayerMover playerMover)
+    {
+        _playerMover = playerMover;
+    }
+
+    public void Execute()
+    {
+        _playerMover.Force += Vector2.right;
+    }
+}
+
+
+public class CommandInvoker
+{
+    private Queue<ICommand> _commands = new Queue<ICommand>();
+
+    public void AddCommand(ICommand command)
+    {
+        _commands.Enqueue(command);
+    }
+
+    public void ExecuteCommands()
+    {
+        while (_commands.Count > 0)
+        {
+            ICommand command = _commands.Dequeue();
+            command.Execute();
+        }
+    }
+}
+
 public class PlayerMover : MonoBehaviour
 {
     [SerializeField] float speed;
@@ -13,13 +98,22 @@ public class PlayerMover : MonoBehaviour
 
     SpriteRenderer spriteRenderer;
     private RewindManager rewindManager;
+    public Vector2 Force
+    {
+        get { return force; }
+        set { force = value; }
+    }
+
+    // Add an instance of the CommandInvoker
+    private CommandInvoker commandInvoker;
 
     private void Awake() {
-        rewindManager = new RewindManager();
+        rewindManager = GetComponent<RewindManager>();
         rigidbody2D = GetComponent<Rigidbody2D>();
         player = GetComponent<Player>();
         rigidbody2D.freezeRotation = true;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        commandInvoker = new CommandInvoker();
     }
     void Update()
     {
@@ -34,24 +128,28 @@ public class PlayerMover : MonoBehaviour
         
     }
 
-    void UpdateForce(){
-        if (!RewindManager.isRewinding){
-            int verticalMove=0;
-            int horizontalMove=0;
-            if(Input.GetKey(KeyCode.W)) verticalMove++;
-            if(Input.GetKey(KeyCode.S)) verticalMove--;
-            if(Input.GetKey(KeyCode.A)) horizontalMove--;
-            if(Input.GetKey(KeyCode.D)) horizontalMove++;
-            
-            force = new Vector2(horizontalMove, verticalMove);
+    void UpdateForce()
+    {
+        if (!RewindManager.isRewinding)
+        {
+            force = Vector2.zero;
 
+            if (Input.GetKey(KeyCode.W))
+                commandInvoker.AddCommand(new MoveUpCommand(this));
+            if (Input.GetKey(KeyCode.S))
+                commandInvoker.AddCommand(new MoveDownCommand(this));
+            if (Input.GetKey(KeyCode.A))
+                commandInvoker.AddCommand(new MoveLeftCommand(this));
+            if (Input.GetKey(KeyCode.D))
+                commandInvoker.AddCommand(new MoveRightCommand(this));
 
-            if(force.x!=0 && player.isAlive && Time.timeScale>0)
+            commandInvoker.ExecuteCommands();
+
+            if (force.x != 0 && player.isAlive && Time.timeScale > 0)
             {
-                spriteRenderer.flipX = force.x<0;
+                spriteRenderer.flipX = force.x < 0;
             }
         }
-        
     }
     
     void OnTriggerEnter2D(Collider2D other) {       
